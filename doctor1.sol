ff48734156eeacf55d360175107652837f820b7f
Rohit Singh <rohit092002g@gmail.com>
2023-08-23 02:10:37 +0530

    Update doctor1.sol

--- doctor1.sol
+++ doctor1.sol
@@ -1,1 +1,33 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.9;
+
+contract doctorContract{
+
+    address public Doctor;
+    string private doctorDetails;
+    mapping(address=>bool) public patientList;
+    uint public patientCount;
+    mapping(uint=>address) public patientTrack;
+    
+
+    constructor(address doctor,string memory docDetails){
+        Doctor = doctor;
+        doctorDetails = docDetails;
+    }
+
+    // first doctor will save the patient hash
+    function recordPatient(address patient) public {
+        patientList[patient] = true;
+        patientTrack[patientCount]  = patient;
+        patientCount+=1;
+
+    }
+
+
+  
+
+
+    
+    
+}

6997012a71078438cbed6bf3c2895223336b5c13
Rohit Singh <rohit092002g@gmail.com>
2023-08-23 02:10:07 +0530

    Update medical1.sol

--- medical1.sol
+++ medical1.sol
@@ -1,1 +1,38 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.9;
+
+contract medicalProfContract{
+    address public Prof;
+    uint public reportCount;
+    mapping(uint=>string) private reportsTrack;
+    mapping(string=>string) private reportComments;
+    mapping(string=>address) private reportPatients;
+
+    constructor(address prof){
+        Prof = prof;
+    }
+
+    modifier onlyProf{
+        require(msg.sender==Prof,"only authorised professionals can access");
+        _;
+    }
+
+    function getReports(uint index) public view onlyProf returns(string memory){
+        return reportsTrack[index];
+
+    }
+
+    function addComments(string memory comments, string memory report) public onlyProf{
+        reportComments[report] = comments;
+    }
+
+
+    function addReports(string memory report,address patient) public {
+        reportsTrack[reportCount] = report;
+        reportCount+=1;
+        reportPatients[report] = patient;
+
+    }
+
+}
